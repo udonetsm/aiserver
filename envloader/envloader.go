de2318a25949bcfdc06e330dd312f7db6a483cfe
udonetsm <donetamaksimm@yandex.ru>
2025-05-06 09:50:53 +0300

    fix panic on start server if flag e is empty or have not valid value

--- envloader/envloader.go
+++ envloader/envloader.go
@@ -2,6 +2,8 @@ package envloader
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -17,11 +19,14 @@ type EnvLoader interface {
 func (e *envLoader) LoadEnvs() error {
 	err := godotenv.Load(e.source)
 	if err != nil {
-		return fmt.Errorf("%w", err)
+		return fmt.Errorf("%w<---", err)
 	}
 	return nil
 }
 
 func NewEnvLoader(source string) EnvLoader {
+	if _, err := os.Stat(source); err != nil {
+		log.Fatal(err)
+	}
 	return &envLoader{source: source}
 }
